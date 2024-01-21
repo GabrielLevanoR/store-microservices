b8886bfe0dfe9341f1d9e64dded3d6af8dc6ea6d
GabrielLevanoR <glevanoalc@gmail.com>
2024-01-21 17:44:24 -0500

    fix ame DB and SQL script

--- service-shopping/src/main/resources/schema.sql
+++ service-shopping/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS tbl_products;
+DROP TABLE IF EXISTS tbl_invoice_items;
 
-CREATE TABLE tbl_invoce_items (
+CREATE TABLE tbl_invoice_items (
     id BIGINT AUTO_INCREMENT  PRIMARY KEY,
     invoice_id BIGINT,
     product_id BIGINT,
@@ -8,9 +8,9 @@ CREATE TABLE tbl_invoce_items (
     price DOUBLE NOT NULL
 );
 
-DROP TABLE IF EXISTS tbl_categories;
+DROP TABLE IF EXISTS tbl_invoices;
 
-CREATE TABLE tlb_invoices (
+CREATE TABLE tbl_invoices (
     id BIGINT AUTO_INCREMENT  PRIMARY KEY,
     number_invoice VARCHAR(250) NOT NULL,
     description VARCHAR(250) NOT NULL,
