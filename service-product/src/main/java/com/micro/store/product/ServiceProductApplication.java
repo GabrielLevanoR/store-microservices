04625bff406cb9d9deb322da1338c9398fa21daa
GabrielLevanoR <glevanoalc@gmail.com>
2024-01-23 15:39:58 -0500

    Admin panel

--- service-product/src/main/java/com/micro/store/product/ServiceProductApplication.java
+++ service-product/src/main/java/com/micro/store/product/ServiceProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+
 public class ServiceProductApplication {
 
 	public static void main(String[] args) {
